29c3b8d4c45d6c93f4249ff4bad3a313a3f1a2a5
ffati <hellol2016@outlook.com>
2020-04-13 19:46:57 +0800

    SpringCloud学习项目

---
+++ microservicecloud-config-service-3344/src/main/java/com/ff/microservicecloudconfig3344/MicroservicecloudConfigService3344Application.java
@@ -0,0 +1,15 @@
+package com.ff.microservicecloudconfig3344;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MicroservicecloudConfigService3344Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudConfigService3344Application.class, args);
+    }
+
+}
